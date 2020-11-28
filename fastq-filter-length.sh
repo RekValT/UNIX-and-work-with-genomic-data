8bd24d36fccf5ec02fc0b2a4b14645e438c53021
Marek Valt <valtm@natur.cuni.cz>
2020-11-29 00:37:39 +0100

    fastq filter

--- fastq-filter-length.sh
+++ fastq-filter-length.sh
@@ -6,6 +6,8 @@ paste - - - - |
  awk -v min_len=$1 '(length ($2) > min_len)' |
  tr "\t" "\n"
 
+ <data/HRTMUOC01.RL12.01.fastq ./fastq-filter-length.sh 800 | wc -l
+
 # to stay with the 'tool concept'
 # expect input on stdin and output the results to stdout
 
